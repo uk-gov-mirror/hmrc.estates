145dda6df39ef52045e64026284c0c778a891fac
Darryl Murphy <darrylmurphy@users.noreply.github.com>
2020-06-10 16:07:08 +0100

    Tests for personal rep controller

--- test/uk/gov/hmrc/estates/controllers/PersonalRepTransformationControllerSpec.scala
+++ test/uk/gov/hmrc/estates/controllers/PersonalRepTransformationControllerSpec.scala
@@ -28,9 +28,9 @@ import play.api.inject.bind
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import uk.gov.hmrc.estates.BaseSpec
-import uk.gov.hmrc.estates.models.{EstatePerRepIndType, IdentificationType, NameType}
+import uk.gov.hmrc.estates.models.{EstatePerRepIndType, EstatePerRepOrgType, IdentificationOrgType, IdentificationType, NameType}
 import uk.gov.hmrc.estates.services.{PersonalRepTransformationService, TransformationService}
-import uk.gov.hmrc.estates.transformers.{AmendEstatePerRepIndTransform, ComposedDeltaTransform}
+import uk.gov.hmrc.estates.transformers.{AmendEstatePerRepIndTransform, AmendEstatePerRepOrgTransform, ComposedDeltaTransform}
 
 import scala.concurrent.Future
 import scala.util.Success
@@ -39,70 +39,109 @@ class PersonalRepTransformationControllerSpec extends BaseSpec with MockitoSugar
 
   private val transformationService = mock[TransformationService]
 
-  "amend personal rep" must {
+  private val personalRepInd = EstatePerRepIndType(
+    name = NameType("First", None, "Last"),
+    dateOfBirth = LocalDate.of(2019, 6, 1),
+    identification = IdentificationType(
+      nino = Some("JH123456C"),
+      passport = None,
+      address = None
+    ),
+    phoneNumber = "07987654345",
+    email = None
+  )
+
+  private val personalRepOrg = EstatePerRepOrgType(
+    orgName =  "Personal Rep Org",
+    identification = IdentificationOrgType(None, None),
+    phoneNumber = "07987654",
+    email = None
+  )
+
+  "amend personal rep ind" must {
 
     "add a new amend personal rep transform" in {
 
-      val personalRep = EstatePerRepIndType(
-        name =  NameType("First", None, "Last"),
-        dateOfBirth = LocalDate.of(2000,1,1),
-        identification = IdentificationType(None, None, None),
-        phoneNumber = "07987654",
-        email = None
-      )
+        val personalRepTransformationService = mock[PersonalRepTransformationService]
 
-      val personalRepTransformationService = mock[PersonalRepTransformationService]
+        val application = applicationBuilder()
+          .overrides(
+            bind[PersonalRepTransformationService].toInstance(personalRepTransformationService)
+          ).build()
 
-      val application = applicationBuilder()
-        .overrides(
-          bind[PersonalRepTransformationService].toInstance(personalRepTransformationService)
-        ).build()
+        when(personalRepTransformationService.addAmendEstatePerRepIndTransformer(any(), any()))
+          .thenReturn(Future.successful(Success))
 
-      when(personalRepTransformationService.addAmendEstatePerRepIndTransformer(any(), any()))
-        .thenReturn(Future.successful(Success))
+        val controller = application.injector.instanceOf[PersonalRepTransformationController]
 
-      val controller = application.injector.instanceOf[PersonalRepTransformationController]
+        val request = FakeRequest("POST", "path")
+          .withBody(Json.toJson(personalRepInd))
+          .withHeaders(CONTENT_TYPE -> "application/json")
 
-      val request = FakeRequest("POST", "path")
-        .withBody(Json.toJson(personalRep))
-        .withHeaders(CONTENT_TYPE -> "application/json")
+        val result = controller.amendPersonalRepInd().apply(request)
 
-      val result = controller.amendPersonalRepInd().apply(request)
+        status(result) mustBe OK
+        verify(personalRepTransformationService)
+          .addAmendEstatePerRepIndTransformer("id", personalRepInd)
+      }
 
-      status(result) mustBe OK
-      verify(personalRepTransformationService)
-        .addAmendEstatePerRepIndTransformer("id", personalRep)
+    "must return an error for malformed json" in {
+
+        val controller = injector.instanceOf[PersonalRepTransformationController]
+
+        val request = FakeRequest("POST", "path")
+          .withBody(Json.parse("{}"))
+          .withHeaders(CONTENT_TYPE -> "application/json")
+
+        val result = controller.amendPersonalRepInd().apply(request)
+        status(result) mustBe BAD_REQUEST
+      }
+  }
+
+  "amend personal rep org" must {
+
+    "add a new amend personal rep transform" in {
+
+        val personalRepTransformationService = mock[PersonalRepTransformationService]
+
+        val application = applicationBuilder()
+          .overrides(
+            bind[PersonalRepTransformationService].toInstance(personalRepTransformationService)
+          ).build()
+
+        when(personalRepTransformationService.addAmendEstatePerRepOrgTransformer(any(), any()))
+          .thenReturn(Future.successful(Success))
+
+        val controller = application.injector.instanceOf[PersonalRepTransformationController]
+
+        val request = FakeRequest("POST", "path")
+          .withBody(Json.toJson(personalRepOrg))
+          .withHeaders(CONTENT_TYPE -> "application/json")
+
+        val result = controller.amendPersonalRepOrg().apply(request)
+
+        status(result) mustBe OK
+        verify(personalRepTransformationService)
+          .addAmendEstatePerRepOrgTransformer("id", personalRepOrg)
     }
 
     "must return an error for malformed json" in {
 
-      val controller = injector.instanceOf[PersonalRepTransformationController]
+        val controller = injector.instanceOf[PersonalRepTransformationController]
 
-      val request = FakeRequest("POST", "path")
-        .withBody(Json.parse("{}"))
-        .withHeaders(CONTENT_TYPE -> "application/json")
+        val request = FakeRequest("POST", "path")
+          .withBody(Json.parse("{}"))
+          .withHeaders(CONTENT_TYPE -> "application/json")
 
-      val result = controller.amendPersonalRepInd().apply(request)
-      status(result) mustBe BAD_REQUEST
+        val result = controller.amendPersonalRepOrg().apply(request)
+        status(result) mustBe BAD_REQUEST
     }
   }
 
-  "getPersonalRep" should {
+  "getPersonalRepInd" should {
 
     "return 200 - Ok with processed content" when {
       "a transform is retrieved" in {
-
-        val personalRep = EstatePerRepIndType(
-          name = NameType("First", None, "Last"),
-          dateOfBirth = LocalDate.of(2019, 6, 1),
-          identification = IdentificationType(
-            nino = Some("JH123456C"),
-            passport = None,
-            address = None
-          ),
-          phoneNumber = "07987654345",
-          email = None
-        )
 
         val application = applicationBuilder()
           .overrides(
@@ -110,15 +149,15 @@ class PersonalRepTransformationControllerSpec extends BaseSpec with MockitoSugar
           ).build()
 
         when(transformationService.getTransformedData(any[String]))
-          .thenReturn(Future.successful(Some(ComposedDeltaTransform(Seq(AmendEstatePerRepIndTransform(personalRep))))))
+          .thenReturn(Future.successful(Some(ComposedDeltaTransform(Seq(AmendEstatePerRepIndTransform(personalRepInd))))))
 
         val controller = application.injector.instanceOf[PersonalRepTransformationController]
 
-        val result = controller.getPersonalRepInd()(FakeRequest(GET, s"/trusts/transformed/personal-rep"))
+        val result = controller.getPersonalRepInd()(FakeRequest(GET, "/estates/personal-rep/individual"))
 
         status(result) mustBe OK
         contentType(result) mustBe Some(JSON)
-        contentAsJson(result) mustBe Json.toJson(personalRep)
+        contentAsJson(result) mustBe Json.toJson(personalRepInd)
 
       }
 
@@ -134,7 +173,52 @@ class PersonalRepTransformationControllerSpec extends BaseSpec with MockitoSugar
 
         val controller = application.injector.instanceOf[PersonalRepTransformationController]
 
-        val result = controller.getPersonalRepInd()(FakeRequest(GET, s"/trusts/transformed/personal-rep"))
+        val result = controller.getPersonalRepInd()(FakeRequest(GET, "/estates/personal-rep/individual"))
+
+        status(result) mustBe OK
+        contentType(result) mustBe Some(JSON)
+        contentAsJson(result) mustBe Json.toJson(Json.obj())
+      }
+
+    }
+  }
+
+  "getPersonalRepOrg" should {
+
+    "return 200 - Ok with processed content" when {
+      "a transform is retrieved" in {
+
+        val application = applicationBuilder()
+          .overrides(
+            bind[TransformationService].toInstance(transformationService)
+          ).build()
+
+        when(transformationService.getTransformedData(any[String]))
+          .thenReturn(Future.successful(Some(ComposedDeltaTransform(Seq(AmendEstatePerRepOrgTransform(personalRepOrg))))))
+
+        val controller = application.injector.instanceOf[PersonalRepTransformationController]
+
+        val result = controller.getPersonalRepOrg()(FakeRequest(GET, "/estates/personal-rep/organisation"))
+
+        status(result) mustBe OK
+        contentType(result) mustBe Some(JSON)
+        contentAsJson(result) mustBe Json.toJson(personalRepOrg)
+
+      }
+
+      "a transform is not retrieved" in {
+
+        val application = applicationBuilder()
+          .overrides(
+            bind[TransformationService].toInstance(transformationService)
+          ).build()
+
+        when(transformationService.getTransformedData(any[String]))
+          .thenReturn(Future.successful(None))
+
+        val controller = application.injector.instanceOf[PersonalRepTransformationController]
+
+        val result = controller.getPersonalRepOrg()(FakeRequest(GET, "/estates/personal-rep/organisation"))
 
         status(result) mustBe OK
         contentType(result) mustBe Some(JSON)

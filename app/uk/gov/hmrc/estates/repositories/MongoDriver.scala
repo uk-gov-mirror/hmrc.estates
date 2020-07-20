5f01e9e314495719572452d820f6dfd630d0b2d6
Jay Nabonne <jaynabonne@gmail.com>
2020-07-20 14:04:17 +0100

    Remove more logging.

--- app/uk/gov/hmrc/estates/repositories/MongoDriver.scala
+++ app/uk/gov/hmrc/estates/repositories/MongoDriver.scala
@@ -21,9 +21,7 @@ import javax.inject.{Inject, Singleton}
 import play.modules.reactivemongo.ReactiveMongoApi
 
 @Singleton
-class EstatesMongoDriver @Inject()(val api : ReactiveMongoApi) extends MongoDriver {
-  println("CREATE EstatesMongoDriver")
-}
+class EstatesMongoDriver @Inject()(val api : ReactiveMongoApi) extends MongoDriver 
 
 @ImplementedBy(classOf[EstatesMongoDriver])
 sealed trait MongoDriver {
